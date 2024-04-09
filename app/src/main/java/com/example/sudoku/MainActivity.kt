e91b4649c1a340ef9e471255c98afb69d0fa4c03
Jimmy Saavedra <jim312002@hotmail.com>
2024-04-09 05:47:23 -0500

    Intento de validacion de sudoku, no valida las matrices

--- app/src/main/java/com/example/sudoku/MainActivity.kt
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.GridLayout
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.sudoku.Logic.CreateBoard
 import com.example.sudoku.Logic.CreateGame
@@ -19,6 +20,7 @@ import com.example.sudoku.databinding.GameActivityBinding
 class MainActivity : ComponentActivity() {
 
     private lateinit var binding: GameActivityBinding
+    private lateinit var game: Array<IntArray>
 
     val randomizer = Randomizer()
     val createBoard = CreateBoard()
@@ -29,12 +31,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         binding = GameActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
         val board = Array(9) { IntArray(9) { 0 } }
         val tablero = randomizer.randomizer(board)
-        val game = createGame.createGame(tablero)
+        game = createGame.createGame(tablero)
         createBoard.createBoard(findViewById<GridLayout>(R.id.gridLayoutTablero), game)
 
         var selectedEditText: EditText? = null
@@ -52,6 +51,7 @@ class MainActivity : ComponentActivity() {
                     if (text.isEmpty()) {
                         selectedEditText = child as EditText
                         child.background = drawable
+                        updateGameValue(i, child.text.toString().toIntOrNull() ?: 0)
                     }
 
                 }
@@ -93,7 +93,7 @@ class MainActivity : ComponentActivity() {
             selectNumber.selectNumber(6, selectedEditText)
             selectedEditText = null
         }
-        val buttonSeven= findViewById<Button>(R.id.sevenButton)
+        val buttonSeven = findViewById<Button>(R.id.sevenButton)
         buttonSeven.setOnClickListener {
             // Establece el número 1 en el EditText seleccionado
             selectNumber.selectNumber(7, selectedEditText)
@@ -111,6 +111,37 @@ class MainActivity : ComponentActivity() {
             selectNumber.selectNumber(9, selectedEditText)
             selectedEditText = null
         }
+
+        val checkButton = findViewById<Button>(R.id.acceptButton)
+        checkButton.setOnClickListener {
+            var isCorrect = true
+            for (i in 0 until 9) {
+                for (j in 0 until 9) {
+                    val index = i * 9 + j
+                    val editText = binding.gridLayoutTablero.getChildAt(index) as EditText
+                    if (game[i][j] != tablero[i][j]) {
+
+                        isCorrect = false
+                        editText.setBackgroundColor(Color.RED)
+                    } else {
+                        editText.setBackgroundColor(Color.GREEN)
+                    }
+                }
+            }
+            if (!isCorrect) {
+                // Mostrar un mensaje al usuario indicando que hay errores
+                Toast.makeText(this, "¡Hay errores en el sudoku!", Toast.LENGTH_SHORT).show()
+            } else {
+                // Mostrar un mensaje al usuario indicando que el sudoku es correcto
+                Toast.makeText(this, "¡El sudoku es correcto!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateGameValue(index: Int, value: Int) {
+        val row = index / 9
+        val col = index % 9
+        game[row][col] = value
 
     }
 }
